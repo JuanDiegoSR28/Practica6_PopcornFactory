ac8439d414be62b82eb04ce45a6653c073fa2435
JuanDiegoSR28 <juan.sanchez233013@potros.itson.edu.mx>
2024-06-17 23:37:24 -0700

    RadioButton para reservar asientos p1

--- app/src/main/java/sanchez/juan/popcornfactory/MovieDetail.kt
+++ app/src/main/java/sanchez/juan/popcornfactory/MovieDetail.kt
@@ -1,6 +1,8 @@
 package sanchez.juan.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -13,19 +15,40 @@ class MovieDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
 
+        var id = -1
         val titulo = intent.getStringExtra("titulo")
         val sinopsis = intent.getStringExtra("sinopsis")
         val header = intent.getIntExtra("header", 0)
+        val seatsLeft = intent.getIntExtra("numberSeats",0)
+        id = intent.getIntExtra("pos",0)
+
 
 
         val headerImageView: ImageView = findViewById(R.id.DetailHeader)
         val titleTextView: TextView = findViewById(R.id.DetailTitle)
         val synopsisTextView: TextView = findViewById(R.id.DetailDescription)
-
+        val seatsLeftTextView : TextView = findViewById(R.id.seatsLeft)
+        val butTicketsButton: Button = findViewById(R.id.buy_tickets)
 
         headerImageView.setImageResource(header)
         titleTextView.text = titulo
         synopsisTextView.text = sinopsis
+        seatsLeftTextView.text = ("$seatsLeft seats available")
+
+        if(seatsLeft == 0 ){
+            butTicketsButton.isActivated = false
+
+        }
+        else{
+            butTicketsButton.setOnClickListener{
+                val intent: Intent = Intent(this,SeatSelection::class.java)
+
+                intent.putExtra("movie", id)
+                intent.putExtra("name", titulo)
+
+            }
+        }
+
 
     }
 }
---
+++ app/src/main/java/sanchez/juan/popcornfactory/SeatSelection.kt
@@ -0,0 +1,15 @@
+package sanchez.juan.popcornfactory
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SeatSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_selection)
+
+    }
+}
